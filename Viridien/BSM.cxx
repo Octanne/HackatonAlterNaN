71639e96177c2280197b3a4fa2fa7fba94632988
jepacor <Jepacor@gmail.com>
2025-01-21 16:30:19 +0100

    Merge auto foireux corrigé

--- Viridien/BSM.cxx
+++ Viridien/BSM.cxx
@@ -108,7 +108,7 @@ real gaussian_box_muller() {
     return distribution(generator);
 }
 
-/*
+
 // Function to calculate the Black-Scholes call option price using Monte Carlo method
 real black_scholes_monte_carlo(real S0, real K, real T, real r, real sigma, real q, ui64 num_simulations) {
     real sum_payoffs = 0.0;
@@ -147,50 +147,8 @@ real black_scholes_monte_carlo(real S0, real K, real T, real r, real sigma, real
     return real_exp(-r * T) * (sum_payoffs / num_simulations);
 
 }
-*/
 
-#include <arm_neon.h>
-#include "neon_mathfun.h"
 
-real black_scholes_monte_carlo(ui64 S0, ui64 K, real T, real r, real sigma, real q, ui64 num_simulations) {
-    real sum_payoffs = 0.0;
-
-#pragma omp parallel reduction(+:sum_payoffs)
-    {
-        // Initialisation du générateur de nombres aléatoires et des paramètres constants
-        XoshiroCpp::Xoshiro256PlusPlus generator(std::random_device{}());
-	boost::normal_distribution<real> distribution(0.0, 1.0);
-
-        real p1 = (r - q - real(0.5f) * sigma * sigma) * T;
-        real p2 = sigma * real_sqrt(T);
-
-        for (ui64 i = 0; i < num_simulations; i += 4) {
-            // Générer 4 nombres aléatoires gaussiens à la fois
-            float Z_array[4] = {
-                distribution(generator),
-                distribution(generator),
-                distribution(generator),
-                distribution(generator),
-            };
-            float32x4_t Z = vld1q_f32(Z_array);
-
-            // Calcul vectorisé de ST pour 4 simulations
-	    float32x4_t ST = vmulq_f32(vdupq_n_f32(S0), exp_ps(vaddq_f32(vdupq_n_f32(p1), vmulq_f32(vdupq_n_f32(p2), Z))));
-
-            // Calcul des payoffs vectorisés
-            float32x4_t payoff = vmaxq_f32(vsubq_f32(ST, vdupq_n_f32(K)), vdupq_n_f32(0.0f));
-
-            // Ajout des payoffs au vecteur de somme
-            float32x2_t sum_low = vget_low_f32(payoff);
-            float32x2_t sum_high = vget_high_f32(payoff);
-	    float total_sum = vaddvq_f32(vcombine_f32(sum_low, sum_high));
-            sum_payoffs += total_sum;
-	}
-    }
-
-    // Retourner le résultat avec la factorisation
-    return real_exp(-r * T) * (sum_payoffs / num_simulations);
-}
 
 
 int main(int argc, char* argv[]) {
